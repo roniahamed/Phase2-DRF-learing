d327ce8f1df35b21bf6cc8d17e3f417d121f0080
roniahamed <mdroniahamed56@gmail.com>
2025-08-11 04:09:44 +0600

    adding Custom User on Admin deshboard

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'is_staff', 'is_active', 'date_joined', 'password')
+    search_fields = ('email', 'first_name', 'last_name')
+    list_filter = ('is_staff', 'is_active')
+    ordering = ('-date_joined',)
+    fieldsets = (
+        (None, {
+            'fields': ('email', 'password')
+        }),
+        ('Personal info', {
+            'fields': ('first_name', 'last_name')
+        }),
+        ('Permissions', {
+            'fields': ('is_staff', 'is_active')
+        }),
+        ('Important dates', {
+            'fields': ('date_joined',)
+        }),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'first_name', 'last_name')}
+        ),
+    )
