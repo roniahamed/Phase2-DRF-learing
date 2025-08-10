835ee85ea0f5c9f065cc1048ecd2e9a1426a5c43
roniahamed <mdroniahamed56@gmail.com>
2025-08-11 03:36:49 +0600

    Custom User Manager

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,26 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('This email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('SuperUser must have is_staff')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('superuser must have is_superuser = True')
+        
+        return self.create_user(email, password, **extra_fields)
 
 
 
