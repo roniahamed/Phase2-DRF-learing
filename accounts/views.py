7c345d80d05cb20bc16532184a448001bf33a49d
roniahamed <mdroniahamed56@gmail.com>
2025-08-11 04:40:22 +0600

    Registration Views

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
+from .serializers import UserRegistrationSerializer
+from .models import User
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+
+class Registration(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserRegistrationSerializer
+    permission_classes = [AllowAny]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data = request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        headers = self.get_success_headers(serializer.data)
+        
